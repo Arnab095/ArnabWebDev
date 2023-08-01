34de561a3c85b954ee084898b714517cd4b2417a
Arnab Jana <134880644+Arnab095@users.noreply.github.com>
2023-08-02 02:06:43 +0530

    Add files via upload

---
+++ FrontEnd/JavaScript/Day 9b(Function)/index.js
@@ -0,0 +1,43 @@
+/*
+function demo() {
+  var a = parseInt(document.getElementById("a").value);
+  var b = parseInt(document.getElementById("b").value);
+
+  alert(a + b);
+  console.log(a + b);
+  let c = document.getElementById("sum");
+  c.innerHTML = "sum of a and b is =>" + (a + b);
+}
+
+function demo1() {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+  function demo2() {
+    var c = 10;
+    var d = 20;
+    console.log("demo2()", c + d);
+  }
+//   console.log("Outside of demo2", c + d);
+  demo2();
+  return demo2;
+}
+
+// console.log(demo1);
+
+var z = demo1();
+console.log(z);
+z();
+*/
+
+
+
+
+
+
+var x=10;
+function demo(){
+    var a = 20;
+    console.log(a);
+}
+demo();
