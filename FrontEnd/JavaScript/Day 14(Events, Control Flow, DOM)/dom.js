6f1a36cdbd5392eba9d886c5ef5f0ea7a34c6417
Arnab Jana <134880644+Arnab095@users.noreply.github.com>
2023-08-07 00:57:42 +0530

    Add files via upload

---
+++ FrontEnd/JavaScript/Day 14(Events, Control Flow, DOM)/dom.js
@@ -0,0 +1,18 @@
+/**
+ * A. Find HTML Elements
+ * document.getElementById(id)
+ * document.getElementsByClassName(name)
+ * document.getElementsByTagName(name)
+ * 
+ * B. Change HTML Elements
+ * element.innerHTML
+ * element.style.property
+ * 
+ * C. Add Elements
+ * document.createElement(element)
+ * document.write(text)
+ * 
+ * D. Delete Elements
+ * document.removeChild(element)
+ * 
+ */
---
+++ FrontEnd/JavaScript/Day 14(Events, Control Flow, DOM)/events.js
@@ -0,0 +1,16 @@
+// function demo() {
+//   alert("Hey!!! Welcome to Demo...");
+// }
+
+function demo() {
+  const ele = document.getElementById("para");
+  ele.innerHTML = "Hey you have over onmouseover events";
+  ele.style.backgroundColor = "azure";
+}
+function demo1() {
+  const ele = document.getElementById("para");
+  ele.style.backgroundColor = "aqua";
+}
+function demo2(val) {
+  alert(val);
+}
