013739fe3cfad71ebf7e40a026bfec5cbd6e4c4a
Arnab Jana <134880644+Arnab095@users.noreply.github.com>
2023-08-13 00:59:30 +0530

    Add files via upload

---
+++ FrontEnd/JavaScript/Day 18b(AJAX)/index.js
@@ -0,0 +1,13 @@
+function loaddynamic() {
+  // alert();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    } else {
+      alert("Error in calls!!");
+    }
+  };
+  xhttp.open("GET", "info.txt", true);
+  xhttp.send();
+}
