f3ac8cb863780d53c9da51beb41d7f373852b06f
Arnab Jana <134880644+Arnab095@users.noreply.github.com>
2023-08-22 23:33:37 +0530

    Add files via upload

---
+++ FrontEnd/JavaScript/Day 22(Oops)/script.js
@@ -0,0 +1,172 @@
+/*
+Person={
+  fName:"John",
+  lName:"Doe",
+  Age:30,
+  hasDivingLicence:true,
+}
+console.log(Person);
+console.log(Person.lName);
+// console.log(Person.fName+" "+Person.lName);
+
+Person1={
+  fname:"John",
+  lName:"Smith",
+  Age:60
+}
+
+console.log(Person.Age=35);
+
+var btn=document.getElementById("btn");
+function onBtnClick(e){
+console.log(e);
+}
+console.log(console);
+
+btn.addEventListener("click",onBtnClick)
+*/
+
+
+
+
+/************************************/
+// Procedural programming language //
+// =============================== /
+// Object Oriented Programming  ///
+/********************************/
+Person={
+  fName:"John",
+  lName:"Doe",
+  Age:30,
+  hasDivingLicence:true,
+  printName:function(){
+    console.log(this.fName+" "+this.lName);
+  }
+}
+console.log(Person);
+console.log(Person.lName);
+// console.log(Person.fName+" "+Person.lName);
+console.log(Person.printName());
+
+console.log(Person.Age=35);
+
+function PersonDet(fName,lName,Age){// constructor function
+ this.fName=fName,
+ this.lName=lName,
+ this.Age=Age ,
+ this.setAge=function(Age){
+  this.Age=Age
+ }
+ this.getAge=function(){
+  return this.Age
+ }
+ this.setfName=function(fName){
+  this.fName=fName
+ }
+ this.getfName=function(){
+  return this.fName
+ }
+}
+
+var person=new PersonDet("Ann","Tom",105);// Object instantiation
+console.log(person);
+
+var person1=new PersonDet("Bella","John",34);
+console.log(person1)
+
+
+console.log(person.getAge())
+person.setAge(50);//Setter method
+console.log(person.getAge())//Getter method
+
+
+// class method
+class PersonDetails{
+  constructor(fName,lName,Age){ //constructor function
+      this.fName=fName,
+      this.lName=lName,
+      this.Age=Age
+  }
+  getAge(){
+      return this.Age
+  }
+  setAge(Age){
+      this.Age=Age
+  }
+  setfName(fName){
+      this.fName=fName
+  }
+  getfName(){
+      return this.fName
+  }
+}
+var person2=new PersonDetails("Athira","Suresh",26);//object instantiation
+console.log(person2)
+person2.setfName("Alice");
+console.log(person2);
+person2.setAge(30)
+console.log(person2)
+
+// 2nd example of Object Oriented Programming
+car={
+  name:"BMW",
+  makeYear:2023,
+  isUsePetrol:true,
+  printYear:function(){
+    console.log(this.makeYear)
+  }
+
+}
+console.log(car.printYear());
+
+
+
+/****************/
+// inheritance //
+/**************/
+class Vechicle {
+  constructor(fuelAmount, capacity) {
+      this.fuelAmount = fuelAmount;
+      this.capacity = capacity;
+  }
+
+  setCapacity(capacity) {
+      this.capacity = capacity;
+  }
+
+  getCapacity() {
+      return this.capacity;
+  }
+
+  setFuelAmount(fuelAmount) {
+      this.fuelAmount = fuelAmount;
+  }
+
+  getFuelAmount() {
+      return this.fuelAmount;
+  }
+}
+
+class Bus extends Vechicle {
+  constructor(fuelAmount, capacity, hasPetrol) {
+      super(fuelAmount, capacity); //Here you have to pass all the parent class properties , which caused the problem of our code
+      this.hasPetrol = hasPetrol;
+  }
+}
+
+var bus = new Bus(150, 100, true); // Pass the fuelAmount, capacity, and hasPetrol values
+console.log(bus.getCapacity()); // Output: 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
